4424b19d0d7d4d1cbdf935782bf50fbe6918f6b2
udeh princewill <princezubby2020@gmail.com>
2022-12-26 11:07:31 +0100

    fix

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
   loader(messgeDiv);
 
   //fetch data from server => Bot response
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://botx.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
